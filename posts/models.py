f9362fda77028c8b1f21385731be7e073bde648a
Vitaly <ryudo14@yandex.ru>
2023-03-11 17:58:22 +0300

    Lesson Django

---
+++ posts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.test import TestCase
+from django.urls import reverse
+
+
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='just a test')
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name=f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a test')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='another test')
+        def test_view_url_exists_at_proper_location(self):
+            resp = self.client.get('/')
+            self.assertEqual(resp.status_code, 200)
+            def test_view_url_by_name(self):
+                resp = self.client.get(reverse('home'))
+                self.assertEqual(resp.status_code, 200)
